e19432bda6716518c061da300a5fc86b8faa9c22
lyaliny <lyaliny@gmail.com>
2022-10-04 17:00:42 -0300

    finalização

--- js/validacao.js
+++ js/validacao.js
@@ -42,11 +42,29 @@ const mensagensDeErro = {
     valueMissing: "O campo de CPF não pode estar vazio.",
     customError: "O CPF digitado não é válido.",
   },
+  cep: {
+    valueMissing: "O campo de CEP não pode estar vazio.",
+    patternMismatch: "O CEP digitado não é válido.",
+    customError: "Não foi possivel buscar o CEP.",
+  },
+  logradouro: {
+    valueMissing: "O campo de logradouro não pode estar vazio.",
+  },
+  cidade: {
+    valueMissing: "O campo cidade não pode estar vazio.",
+  },
+  estado: {
+    valueMissing: "O campo estado não pode estar vazio.",
+  },
+  preco: {
+    valueMissing: "O campo preço não pode estar vazio.",
+  },
 };
 
 const validadores = {
   validaDataNascimento: (input) => validaDataNascimento(input),
   cpf: (input) => validaCPF(input),
+  cep: (input) => recuperarCEP(input),
 };
 
 function mostraMensagemDeErro(tipoDeInput, input) {
@@ -140,4 +158,40 @@ function checaDigitoVerificador(cpf, multiplicador) {
 
 function confirmaDigito(soma) {
   return 11 - (soma % 11);
+}
+
+function recuperarCEP(input) {
+  const cep = input.value.replace(/\D/g, "");
+
+  const url = `https://viacep.com.br/ws/${cep}/json/`;
+  const options = {
+    method: "GET",
+    mode: "cors",
+    headers: {
+      "content-type": "application/json;charset=utf-8",
+    },
+  };
+  if (!input.validity.patternMismatch && !input.validity.valueMissing) {
+    fetch(url, options)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.erro) {
+          input.setCustomValidity("Não foi possivel buscar o cep.");
+          return;
+        }
+        input.setCustomValidity("");
+        preencheCampoComCEP(data);
+        return;
+      });
+  }
+}
+
+function preencheCampoComCEP(data) {
+  const logradouro = document.querySelector('[data-tipo="logradouro"]');
+  const cidade = document.querySelector('[data-tipo="cidade"]');
+  const estado = document.querySelector('[data-tipo="estado"]');
+
+  logradouro.value = data.logradouro;
+  cidade.value = data.localidade;
+  estado.value = data.uf;
 }
